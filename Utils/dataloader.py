5e1efd9bb07d75c3b6d31ea914608ff78bbbe0ce
Tingqi Wang <twang356@usc.edu>
2024-05-03 13:38:54 -0700

    Created new dataloader for test set

--- Utils/dataloader.py
+++ Utils/dataloader.py
@@ -1,10 +1,15 @@
 import torch
 from torchvision import datasets, transforms
 from torch.utils.data import DataLoader
+import os
 
 from Utils.augmentation import *
 
-DIRECTORY = '/Users/tingqiwang/Documents/USC/Spring 2024/CSCI 467/Homework/Project/Practical_Datasets/small_deepfake_and_real_images_subset/'
+base_path = os.path.dirname(os.path.abspath("Project"))
+
+DIRECTORY = os.path.join(base_path, 'Practical_Datasets', '50k_deepfake_and_real_images_subset/') # Change to path of deepfake_and_real_images subset
+
+TEST_DIRECTORY = os.path.join(base_path, 'Practical_Datasets', 'deepfake_faces_test/') # Change to path of deepfake_faces subset
 
 # Adjust the paths based on your dataset structure
 train_data = datasets.ImageFolder(root=DIRECTORY + 'Train', 
@@ -14,27 +19,23 @@ dev_data = datasets.ImageFolder(root=DIRECTORY + 'Validation',
 test_data = datasets.ImageFolder(root=DIRECTORY + 'Test',
                                  transform=deepfake_and_real_images_val_test_transforms)
 
-# # Then ensure all samples use this new mapping
-# train_data.samples = [(s, 1 - idx) for s, idx in train_data.samples]
-# dev_data.samples = [(s, 1 - idx) for s, idx in train_data.samples]
-# test_data.samples = [(s, 1 - idx) for s, idx in train_data.samples]
+deepfake_faces_test_data = datasets.ImageFolder(root=TEST_DIRECTORY,
+                                 transform=deepfake_faces_val_test_transforms)
 
-train_loader = DataLoader(train_data, batch_size=32, shuffle=True)
-dev_loader = DataLoader(dev_data, batch_size=32, shuffle=False)
-test_loader = DataLoader(test_data, batch_size=32, shuffle=False)
 
-# batch_size = 32  # Adjust based on your GPU memory
-# train_loader = DataLoader(train_data, batch_size=batch_size, shuffle=True, num_workers=4)
-# val_loader = DataLoader(val_data, batch_size=batch_size, shuffle=False, num_workers=4)
-# test_loader = DataLoader(test_data, batch_size=batch_size, shuffle=False, num_workers=4)
 
+# train_loader = DataLoader(train_data, batch_size=64, shuffle=True, num_workers=4, pin_memory=True, prefetch_factor=2)
+# dev_loader = DataLoader(dev_data, batch_size=64, shuffle=False, num_workers=4, pin_memory=True, prefetch_factor=2)
+# test_loader = DataLoader(test_data, batch_size=64, shuffle=False, num_workers=4, pin_memory=True, prefetch_factor=2)
+
+# deepfake_faces_test_loader = DataLoader(deepfake_faces_test_data, batch_size=64, shuffle=False, num_workers=4, pin_memory=True, prefetch_factor=2)
+
+train_loader = DataLoader(train_data, batch_size=32, shuffle=True, num_workers=4, pin_memory=True, prefetch_factor=2)
+dev_loader = DataLoader(dev_data, batch_size=32, shuffle=False, num_workers=4, pin_memory=True, prefetch_factor=2)
+test_loader = DataLoader(test_data, batch_size=32, shuffle=False, num_workers=4, pin_memory=True, prefetch_factor=2)
+
+deepfake_faces_test_loader = DataLoader(deepfake_faces_test_data, batch_size=32, shuffle=False, num_workers=4, pin_memory=True, prefetch_factor=2)
+
+# Used for hyperparameter tuning
 def create_data_loader(data, batch_size, shuffle):
-    """
-    Create a DataLoader for the given data.
-    
-    :param data: Dataset object.
-    :param batch_size: Size of each batch.
-    :param shuffle: Whether to shuffle the data.
-    :return: DataLoader with the specified batch size and shuffle settings.
-    """
-    return DataLoader(data, batch_size=batch_size, shuffle=shuffle)
+    return DataLoader(data, batch_size=batch_size, shuffle=shuffle, num_workers=4, pin_memory=True, prefetch_factor=2)
