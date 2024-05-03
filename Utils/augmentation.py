6da9e156a385251899e4f33cd65033b6761bae3f
Tingqi Wang <twang356@usc.edu>
2024-05-03 13:38:35 -0700

    Included data augmentation

--- Utils/augmentation.py
+++ Utils/augmentation.py
@@ -3,32 +3,22 @@ from torchvision import datasets, transforms
 from torch.utils.data import DataLoader
 
 
-# Define transforms for the training, validation, and test sets for deepfake_faces dataset
-deepfake_faces_train_transforms = transforms.Compose([
-    transforms.RandomResizedCrop(224),
-    transforms.RandomHorizontalFlip(),
-    transforms.ToTensor(),
-    transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])  # ImageNet standards
-])
-
 deepfake_faces_val_test_transforms = transforms.Compose([
     transforms.Resize(256),
-    transforms.CenterCrop(224),
     transforms.ToTensor(),
-    transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]) # ImageNet standard weights
 ])
 
-# Define transforms for the training, validation, and test sets for deepfake_and_real_images dataset
+# Augmentation for training set taken from deepfake_and_real_images
 deepfake_and_real_images_train_transforms = transforms.Compose([
-    # transforms.RandomResizedCrop(256),
-    # transforms.RandomHorizontalFlip(),
+    transforms.RandomHorizontalFlip(p=0.5),  # Flip 50% of the time
+    transforms.RandomVerticalFlip(p=0.5),  # Flip 50% of the time
+    transforms.RandomRotation(degrees=30),  # Rotate images by up to 30 degrees in either direction
     transforms.ToTensor(),
-    transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])  # ImageNet standards
+    transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])  # ImageNet standard weights
 ])
 
 deepfake_and_real_images_val_test_transforms = transforms.Compose([
-    # transforms.Resize(256),
-    # transforms.CenterCrop(256),
     transforms.ToTensor(),
-    transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]) # ImageNet standard weights
 ])
